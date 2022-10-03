3539ad48b996cfafa65ca55753d0e337748ab901
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2022-10-03 22:54:58 +0200

    correcting setTimeout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 function Footer({ date, today, showDate, handleClick }) {
   return (
-    <div className='footer'>
+    <>
       {showDate && (
-        <div className='date-container' onClick={handleClick}>
-          {date} {today}
+        <div className='footer'>
+          <div className='date-container' onClick={handleClick}>
+            {date} {today}
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
 
